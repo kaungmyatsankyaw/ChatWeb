0140ae85ace30d8e65a95bc87dfb5c7ea428bf80
kaungmyatsan <kaungmyatsan154996@gmail.com>
2018-12-26 23:18:08 +0630

    Lcation and Message List

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,22 @@
 var socket = io();
 
+
+
+
 socket.on('connect', () => {
     console.log('Connect to Server');
+});
+
+socket.on('newMail', (data) => {
+    console.log(data);
+    var li=jQuery('<li></li>');
+    li.text(data.from + ':' + data.text );
+    $('#messagelist').append(li);
 });
 
 socket.on('disconnect', () => {
     console.log('Disconnect from server');
 });
-
-socket.on('newMail', function (message) {
-    console.log('newMail', message);
-});
-
-
 
 $('#message_form').on('submit', (e) => {
     e.preventDefault();
@@ -23,5 +27,22 @@ $('#message_form').on('submit', (e) => {
     }, (data) => {
         console.log('Thanks Client');
     });
+
+});
+
+
+
+jQuery('#sendLocation').on('click',(e)=>{
+    e.preventDefault;
+
+    if(!navigator.geolocation){
+        return console.log('Geolocation Not Support');
+    }
+    
+    navigator.geolocation.getCurrentPosition((postion)=>{
+        console.log(postion);
+    },()=>{
+        console.log('Cannot Get Location');
+    })
 
 });
